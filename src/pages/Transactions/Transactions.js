46f9fa0eff2ffaaaa3b3e10cd4c19793d18d8d7d
Tiago Cunha <tiago92cunha@gmail.com>
2021-04-08 23:32:33 +0100

    Add form to insert new transaction WIP

--- src/pages/Transactions/Transactions.js
+++ src/pages/Transactions/Transactions.js
@@ -10,7 +10,9 @@ function Transactions() {
          {file.path} - {file.size} bytes
       </li>
    ));
-
+/*  border: .0625rem solid #d1d7e0;
+    box-shadow: 0 0 0 .2rem rgba(#262B40, .25);
+    border-color: lighten(#262B40, 25%) !important;*/
    return (
       <div className="mainPagesContainer">
          <span className="pageTitle">Transactions</span>
@@ -19,11 +21,29 @@ function Transactions() {
                <span className='fileUploadTitle'>File Upload</span>
                <div {...getRootProps({ className: 'transactionFileUpload' })}>
                   <input {...getInputProps()} />
-                  <p>Drag 'n' drop some files here, or click to select files</p>
+                  <p style={{padding: "20px"}}>Drag 'n' drop some files here, or click to select files</p>
                </div>
             </div>
-            <div className="gridTransation">
+            <div className='gridTransation'>
                <span className='fileUploadTitle'>Single Upload</span>
+               <div className='transactionForm'>
+                  <span>Description</span>
+                  <input type="text" className='inputFormTrans' placeholder="Description"></input>
+                  <span>Value</span>
+                  <input type="text" className='inputFormTrans' placeholder="Value"></input>
+                  <span>Category</span>
+                  <select className="categoryList" name="country">
+                     <option value="category1">Category 1</option>
+                     <option value="category2">Category 2</option>
+                     <option value="category3">Category 3</option>
+                  </select>
+                  <span>Sub-Category</span>
+                  <select className="categoryList" name="country">
+                     <option value="subCategory1">Sub Category 1</option>
+                     <option value="subCategory2">Sub Category 2</option>
+                     <option value="subCategory3">Sub Category 3</option>
+                  </select>
+               </div>
             </div>
          </div>
       </div>
