4313ff1cb3131fbee56bdb6fb7d0881b4fb5982e
Tiago Cunha <tiago92cunha@gmail.com>
2021-04-12 21:52:14 +0100

    Add table component to list new Transactions

--- src/pages/Transactions/Transactions.js
+++ src/pages/Transactions/Transactions.js
@@ -3,8 +3,16 @@ import "./styles.css";
 import { useDropzone } from "react-dropzone";
 import { InputText } from "primereact/inputtext";
 import { Dropdown } from "primereact/dropdown";
-import { Button } from 'primereact/button';
+import { Button } from "primereact/button";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 
+const data = [
+   { description: "sadsadasda", value: "99.99", category: "Category 1", subCategory: "Sub category 1" },
+   { description: "sadsadasda", value: "99.99", category: "Category 1", subCategory: "Sub category 1" },
+   { description: "sadsadasda", value: "99.99", category: "Category 1", subCategory: "Sub category 1" },
+   { description: "sadsadasda", value: "99.99", category: "Category 1", subCategory: "Sub category 1" }
+];
 
 const categorySelectItems = [
    { label: "New York", value: "NY" },
@@ -28,6 +36,7 @@ function Transactions() {
    const [subCategory, setSubCategory] = useState(null);
    const [description, setDescription] = useState(null);
    const [value, setValue] = useState(null);
+   const [data, setData] = useState([]);
 
    const files = acceptedFiles.map((file) => (
       <li key={file.path}>
@@ -39,7 +48,6 @@ function Transactions() {
       setCategory(event.value);
    }
 
-   
    function subCategoryHandler(event) {
       setSubCategory(event.value);
    }
@@ -52,24 +60,22 @@ function Transactions() {
       setValue(event.target.value);
    }
 
-   function clearAll(){
+   function clearAll() {
       setCategory(null);
       setSubCategory(null);
       setDescription("");
       setValue("");
    }
 
-   function saveTransaction(){
+   function saveTransaction() {
       const transaction = {
          description: description,
          value: value,
          category: category,
          subCategory: subCategory
-      }
-
-      console.log(transaction);
+      };
+      setData([...data, transaction]);
    }
-   
 
    return (
       <div className="mainPagesContainer">
@@ -86,9 +92,21 @@ function Transactions() {
                <span className="fileUploadTitle">Single Upload</span>
                <div className="transactionForm">
                   <span className="titleTransactionFormFirstodList">Description</span>
-                  <InputText value={description} type="text" className="inputFormTrans" placeholder="Description" onChange={descriptionHandler}></InputText>
+                  <InputText
+                     value={description}
+                     type="text"
+                     className="inputFormTrans"
+                     placeholder="Description"
+                     onChange={descriptionHandler}
+                  ></InputText>
                   <span className="titleTransactionForm">Value</span>
-                  <InputText  value={value} type="text" className="inputFormTrans" placeholder="Value" onChange={valueHandler}></InputText>
+                  <InputText
+                     value={value}
+                     type="text"
+                     className="inputFormTrans"
+                     placeholder="Value"
+                     onChange={valueHandler}
+                  ></InputText>
                   <span className="titleTransactionForm">Category</span>
                   <Dropdown
                      value={category}
@@ -104,17 +122,47 @@ function Transactions() {
                      className="categoryList"
                      options={subCategorySelectItems}
                      onChange={subCategoryHandler}
-                     placeholder="Select a Category"
+                     placeholder="Select a SubCategory"
                      scrollHeight="300px"
                   />
                   <div className="transactionActionButtons">
-                     <Button className='transactionButton' label="Clear" onClick={clearAll}/>
-                     <Button className='transactionButton' label="Save" onClick={saveTransaction}/>
+                     <Button className="transactionButton" label="Clear" onClick={clearAll} />
+                     <Button className="transactionButton" label="Save" onClick={saveTransaction} />
                   </div>
-                 
                </div>
             </div>
          </div>
+         {data.length >0 && <div className='tableTransactionSection'>
+            <DataTable
+               value={data}
+               editMode="row"
+               dataKey="id"
+               //onRowEditInit={onRowEditInit}
+               // onRowEditCancel={onRowEditCancel}
+            >
+               <Column
+                  field="description"
+                  header="Description" /*editor={(props) => codeEditor("products3", props)}*/
+               ></Column>
+               <Column
+                  field="category"
+                  header="Category" /*editor={(props) => nameEditor("products3", props)}*/
+               ></Column>
+               <Column
+                  field="subCategory"
+                  header="SubCategory"
+                  // body={statusBodyTemplate}
+                  /* editor={(props) => statusEditor("products3", props)}*/
+               ></Column>
+               <Column
+                  field="value"
+                  header="value"
+                  // body={priceBodyTemplate}
+                  /* editor={(props) => priceEditor("products3", props)}*/
+               ></Column>
+               <Column rowEditor headerStyle={{ width: "7rem" }} bodyStyle={{ textAlign: "center" }}></Column>
+            </DataTable>
+         </div>}
       </div>
    );
 }
