8abf9d96448b90d36b87a0660c2fb885ddae9860
Tiago Miguel Cunha <tiago.miguel.cunha@celfocus.com>
2021-04-22 18:08:05 +0100

    Add personal info widget

--- src/components/PersonalInfoWidget/index.js
+++ src/components/PersonalInfoWidget/index.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./styles.css";
+import { InputText } from "primereact/inputtext";
 
+function PersonalInfoWidget() {
+   const [firstName, setFirstName] = useState("");
 
-function PersonalInfoWidget () {
-    return <div className='personalInfoContainer'>
-      Hello
-    </div>
-  }
+   return (
+      <div className="personalInfoContainer">
+         <span className="generalInfoMainTitle">General Information</span>
+         <div className="generalInfoSection">
+            <div className="generalInfoGridLeft">
+               <span className="infoTitles">First Name</span>
+               <InputText
+                  value={firstName}
+                  className="generalInfoInputs" /*onChange={(e) => this.setState({value1: e.target.value})} */
+               />
+               <span className="infoTitles">Birthday</span>
+               <InputText
+                  value={firstName}
+                  className="generalInfoInputs" /*onChange={(e) => this.setState({value1: e.target.value})} */
+               />
+               <span className="infoTitles">Email</span>
+               <InputText
+                  value={firstName}
+                  className="generalInfoInputs" /*onChange={(e) => this.setState({value1: e.target.value})} */
+               />
+            </div>
+            <div className="generalInfoGridRight">
+               <span className="infoTitles">Last Name</span>
+               <InputText
+                  value={firstName}
+                  className="generalInfoInputs" /*onChange={(e) => this.setState({value1: e.target.value})} */
+               />
+               <span className="infoTitles">First Name</span>
+               <InputText
+                  value={firstName}
+                  className="generalInfoInputs" /*onChange={(e) => this.setState({value1: e.target.value})} */
+               />
+               <span className="infoTitles">First Name</span>
+               <InputText
+                  value={firstName}
+                  className="generalInfoInputs" /*onChange={(e) => this.setState({value1: e.target.value})} */
+               />
+            </div>
+         </div>
+      </div>
+   );
+}
 
 export default PersonalInfoWidget;
