04da582a26bae4cb09926575a8a1318374b9612f
Tiago Cunha <tiago92cunha@gmail.com>
2021-05-10 23:11:58 +0100

    Create ListCategoriesWidget

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -3,14 +3,21 @@ import PersonalInfoWidget from "../../components/PersonalInfoWidget";
 import "./styles.css";
 import CreateCategoryWidget from "../../components/CreateCategoryWidget";
 import CreateSubCategoryWidget from "../../components/CreateSubCategoryWidget";
-
+import ListCategoriesWidget from "../../components/ListCategoriesWidget";
 function Settings() {
    return (
       <div className="mainPagesContainer">
          <span className="pageTitle">Settings</span>
          <PersonalInfoWidget />
-         <CreateCategoryWidget />
-         <CreateSubCategoryWidget />
+         <div className="categoriesWidgetsContainer">
+            <div className="categoriesWidgetsContainerGrid">
+               <CreateCategoryWidget />
+               <CreateSubCategoryWidget />
+            </div>
+            <div className="categoriesWidgetsContainerGrid">
+               <ListCategoriesWidget />
+            </div>
+         </div>
       </div>
    );
 }
