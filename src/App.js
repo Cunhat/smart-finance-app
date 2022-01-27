b68153399d536a07cb8a2187f4546b8c034ff2a6
Tiago Cunha <50919480+Cunhat@users.noreply.github.com>
2022-01-27 22:30:34 +0000

    Fix layout issues WIP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { NotificationContainer } from 'react-notifications';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import Sidebar from './components/sideBar';
+import { NotificationContainer } from 'react-notifications';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Settings from './pages/Settings/index';
 import Transactions from './pages/Transactions/Transactions';
-import TopBar from './components/topBar';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
@@ -18,44 +16,31 @@ import History from './pages/History';
 import Login from './pages/Login';
 
 function App() {
-  const [hideSideBar, setHideSidebar] = useState(true);
-  const [logedIn] = useState(true);
   const queryClient = new QueryClient();
 
-  function hideBarHandler() {
-    setHideSidebar(!hideSideBar);
-  }
   return (
     <QueryClientProvider client={queryClient}>
       <CategoriesInfoContextProvider>
         <ExpensesContextProvider>
-          <div className="app-container">
+          <div>
             <BrowserRouter>
               <Routes>
                 <Route path="/login" element={<Login />} />
-                {logedIn && (
-                  <>
-                    <Sidebar hideSideBar={hideSideBar}></Sidebar>
-                    <main className="mainContainer">
-                      <TopBar showHideSideBar={hideBarHandler} />
-                      <Route path="/" element={<Dashboard />} />
-                      <Route path="/transactionsHistory" element={<History />} />
-                      <Route path="/transactions" element={<Transactions />} />
-                      <Route path="/settings" element={<Settings />} />
-                      <Route
-                        path="*"
-                        element={
-                          <main style={{ padding: '1rem' }}>
-                            <p>There is nothing here!</p>
-                          </main>
-                        }
-                      />
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/transactionsHistory" element={<History />} />
+                <Route path="/transactions" element={<Transactions />} />
+                <Route path="/settings" element={<Settings />} />
+                <Route
+                  path="*"
+                  element={
+                    <main style={{ padding: '1rem' }}>
+                      <p>There is nothing here!</p>
                     </main>
-                    <NotificationContainer />
-                  </>
-                )}
+                  }
+                />
               </Routes>
             </BrowserRouter>
+            <NotificationContainer />
           </div>
         </ExpensesContextProvider>
       </CategoriesInfoContextProvider>
--- src/components/PageContainer/index.js
+++ src/components/PageContainer/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import PropTypes from 'prop-types';
+import Sidebar from '../sideBar';
+import TopBar from '../topBar';
 
 function PageContainer({ children, ...restProps }) {
+  const [hideSideBar, setHideSidebar] = useState(true);
+
+  function hideBarHandler() {
+    setHideSidebar(!hideSideBar);
+  }
   return (
     <div className="pageContainer" {...restProps}>
-      {children}
+      <Sidebar hideSideBar={hideSideBar}></Sidebar>
+      <main className="mainContainer">
+        <TopBar showHideSideBar={hideBarHandler} />
+        {children}
+      </main>
     </div>
   );
 }
